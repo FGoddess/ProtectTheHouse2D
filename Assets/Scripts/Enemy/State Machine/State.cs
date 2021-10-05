a2e4c6ee1e371c6e7eb3d311ec32d2e213f51fd5
Николай Еремеев <kolan.eremeev1001@gmail.com>
2021-10-06 02:27:01 +0300

    init

---
+++ Assets/Scripts/Enemy/State Machine/State.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+    [SerializeField] private List<Transition> _transitions;
+
+    protected Player Target { get; set; }
+}
